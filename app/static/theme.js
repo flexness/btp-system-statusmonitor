7d25b63e343072fc289d311b44e06fa8c36ea77b
flexness <f.ranneberg@web.de>
2024-07-04 18:52:29 +0200

    update table/module-templates for tags/dependencies; darkmode switch

--- app/static/theme.js
+++ app/static/theme.js
@@ -2,9 +2,11 @@ document.getElementById('btnSwitch').addEventListener('click',()=>{
     if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
         document.documentElement.setAttribute('data-bs-theme','light')
         localStorage.setItem('color-theme', 'light');
+        document.getElementById("flexSwitchCheckDefault").checked = false;
     }
     else {
         document.documentElement.setAttribute('data-bs-theme','dark');
         localStorage.setItem('color-theme', 'dark');
+        document.getElementById("flexSwitchCheckDefault").checked = true;
     }
 })
